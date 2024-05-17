90b2ebbf13fdfadbcb1be4e89487e805d0aac4a7
Ranashazil <shazilr81@gmail.com>
2024-05-17 17:52:05 +0000

    Array concepts

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,44 @@
+// Arrays
+
+const Arrays = [1, 2, 3, 4, 5]
+
+// const myArr = ["one", "two", "three"]
+
+
+
+const Arr = new Array(1, 2, 3, 4, 5, 6, 7)
+console.log(Arr);
+
+// Arrays 
+ Arr.push(6)
+console.log(Arr);
+//  Arr.pop()
+//  Arr.unshift(6)
+//  Arr.shift()
+//  console.log(Arr);
+// console.log(Arr.includes(3));
+console.log(Arr.indexOf(5));
+
+// let newArr = Arr.join()
+// console.log(Arr);
+// console.log(typeof newArr);
+
+// console.log("A ", Arr)
+
+// const newOne = Arr.slice(1, 3)
+// console.log("B ", Arr)
+// console.log(newOne);
+// const newTwo = Arr.splice(1, 3)
+// console.log("C ", Arr)
+// console.log(newTwo);
+
+// const one = Arr.slice(1, 3)
+// console.log("A ", Arr);
+// console.log(one)
+
+
+// const two = Arr.splice(1, 3)
+// console.log("B", Arr);
+// console.log(two);
+
+
