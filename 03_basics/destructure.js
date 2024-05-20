0d338a5ec05927fad181aa44e93e7e45aec65a06
Ranashazil <shazilr81@gmail.com>
2024-05-20 07:36:07 +0000

    deStructuring

---
+++ 03_basics/destructure.js
@@ -0,0 +1,30 @@
+const why = {
+    course: "Web",
+    name: "shazishiza",
+    number: 2324
+}
+
+const {course: C} = why // this is de structuring 
+console.log(C)
+
+// const Navbar = ({item})=>{
+
+// }   // ye abi k liye nai ha ye just smjhnae k liye tha k ese props leete hain
+
+
+// Navbar(item = "none")
+
+{   // this whole is a Api method of JSON / ese b Api mil Sakta ha object ki surat mein // whole Api is in a json formattor
+    "hey" = "what",
+    "you" = "nice", 
+    "extreme" = "available"
+    "isLoggedIn" = true 
+}
+
+[ // ese b Api mil Sakta ha Array ki surat mein // whole Api is in a json formattor
+    {},
+    {},
+    {}
+]
+
+
