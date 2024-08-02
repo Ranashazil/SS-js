f79922ed68c11b0c3c77cdfed58fbcd61e74690d
Ranashazil <shazilr81@gmail.com>
2024-08-02 05:38:55 +0000

    Whole Story of Fetch ..

---
+++ fetch/fetchConcept.js
@@ -0,0 +1,17 @@
+                    // some important concept about fetch//////
+
+
+// 1- fetch ma jo b likha hoga wo sab se phle print hoga kkun k ek special queue hoti ha jis ma ye sab kaam ho ra hita ha or wo special queue fetch k lye hoti ha to sab se phle ye print hone k liye call stack ma jata ha or call stack isko sab se phle print krdeti ha beshak iska syntax sab se last m hi kiun na likha ho or jo phle likhe hote hain wo bad mma print hote ........ so thats why we use fetch.....
+
+
+// 2- fetch ^2 cheezon ko control krta ha ya ap keh skte hain k 2 types hoti hain uski ek to fetch web browsers or node user ki control krta ha or ek data wagera ko resolve krne k liye use hota ha .... web  browser k resouce se network request/node js hoti ha ... jisko fetch control krta ha kuch is triqa se k jo data resolve krte hain wahan p onfullfilled or onrejection hota ha jo k data ki value ko krrta ha fullfiled or network request p node js ya web  browser resource k bad ya to network request p data jayga ya to ni jyga agar ja ker koi b respose ayega wo seedha jayga onFullfilled ma or agar ni jayga to wo rejection ma ayega or agar rewuest gyi hi ni kisi waja se to us case ma rejetion aYEGA 
+
+
+// 3- or be remember one thing k 404 error jo ha netowrk p reqiest gyi ha tb hi to apko pta laga ha ye error aya ha to us case ma ye onfullfilled ma jayega 
+
+
+// 4- or jo data hota HA wo undefined or empty hota ha or iske neeche onfukkfilled ma or rejection ma hote hain hn funtion or ye data ko fullfilled krte hain ek bar date ki value fullfill hogyi to hmra variable kia tha response jo k globbal memory ma b majood ha to ye data ki responsiblity ha k ja kr us varible jo k response ha usko fullfill kre......
+
+
+
+                         // **********thats the story of fetch***************//
